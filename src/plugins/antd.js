942bb782dea5f56bd39cf83b50c96dd93893e085
yangyk520 <237136432@qq.com>
2022-08-25 16:31:28 +0800

    初始化页面顶部组件；页签组件

--- src/plugins/antd.js
+++ src/plugins/antd.js
@@ -1,10 +1,14 @@
 // 按需加载 ant-design-vue
 // 手动删除没用用过的组件
-// import Vue from 'vue';
-// import 'ant-design-vue/lib/button/style/css';
-// import {
-//   Button
-// } from 'ant-design-vue';
+import Vue from 'vue';
+import 'ant-design-vue/lib/button/style/css';
+import {
+  Button,
+  Tabs,
+  Input
+} from 'ant-design-vue';
 
 /* v1.1.3+ registration methods */
-// Vue.use(Button);
+ Vue.use(Button);
+ Vue.use(Tabs);
+ Vue.use(Input);
