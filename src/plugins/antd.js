6dba9dd8c8b8d8f0a3aaf43b736366740d4be7ad
周子正 <ZeroCLemonTea@163.com>
2022-08-30 18:19:33 +0800

    文章详情

--- src/plugins/antd.js
+++ src/plugins/antd.js
@@ -2,13 +2,24 @@
 // 手动删除没用用过的组件
 import Vue from 'vue';
 import 'ant-design-vue/lib/button/style/css';
+import 'ant-design-vue/lib/tabs/style/css';
+import 'ant-design-vue/lib/input/style/css';
+import 'ant-design-vue/lib/spin/style/css';
+import 'ant-design-vue/lib/empty/style/css';
+import 'ant-design-vue/lib/icon/style/css';
 import {
   Button,
   Tabs,
-  Input
+  Input,
+  Spin,
+  Empty,
+  Icon
 } from 'ant-design-vue';
 
 /* v1.1.3+ registration methods */
  Vue.use(Button);
  Vue.use(Tabs);
  Vue.use(Input);
+ Vue.use(Spin);
+ Vue.use(Empty);
+ Vue.use(Icon);
